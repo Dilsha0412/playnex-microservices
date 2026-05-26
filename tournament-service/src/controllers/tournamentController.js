f74e1905c8374d530992cc008160891ae8bb036c
Dilsha Jayasekara <dilshajayasekara2024@gmail.com>
2026-05-27 03:16:32 +0530

    feat: initialize user and tournament services with basic CRUD and inter-service communication logic

--- tournament-service/src/controllers/tournamentController.js
+++ tournament-service/src/controllers/tournamentController.js
@@ -1,4 +1,5 @@
 const tournamentService = require('../services/tournamentService');
+const axios = require('axios');
 
 // Create Tournament
 exports.createTournament = async (req, res) => {
@@ -15,8 +16,17 @@ exports.joinTournament = async (req, res) => {
     const { tournamentId, userId } = req.body;
 
     try {
+
+        try {
+            const userResponse = await axios.get(`http://localhost:5000/api/users/${userId}`);
+            console.log("User Found:", userResponse.data);
+        } catch (axiosError) {
+            return res.status(404).json({ error: "User not found in User Service. Join failed!" });
+        }
+
         const tournament = await tournamentService.joinTournament(tournamentId, userId);
         res.json(tournament);
+
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
